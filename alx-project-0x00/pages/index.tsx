de1603815a4e443082882ce8bc87b5028e7b1c80
NdumisoTyam <Ndumisotyam1@gmail.com>
2025-07-22 20:45:17 +0200

    Update index.tsx

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -17,6 +17,13 @@ const Home: React.FC = () => {
       className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
     >
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1 className="text-4xl font-semibold text-center sm:text-left">
+          Airbnb Application Clone system
+        </h1>
+        <button className="border px-3 py-1 text-lg mt-3 bg-blue-500 text-white rounded-full">
+          Get Started
+        </button>
+
         <Image
           className="dark:invert"
           src="/next.svg"
