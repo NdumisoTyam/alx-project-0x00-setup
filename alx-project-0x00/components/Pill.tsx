62162a32cf9b3c6119b4dbd7613289a80896d646
NdumisoTyam <Ndumisotyam1@gmail.com>
2025-07-25 17:32:39 +0200

    Update Pill.tsx

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,3 +1,4 @@
+import { PillProps } from "@/interfaces";
 const Pill: React.FC = () => {
   return (
     <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
